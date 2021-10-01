69b59761ee248c8b00aad1c8594184b56094f588
JamiHam <82803580+JamiHam@users.noreply.github.com>
2021-10-01 09:39:03 +0300

    Updated search

---
+++ js/search.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const searchInput = document.querySelector("#searchInput");
+const searchButton = document.querySelector("#searchButton");
+const resultsDiv = document.querySelector("#results");
+
+// Event listener for search button
+searchButton.addEventListener("click", () => {
+  resultsDiv.innerHTML = ``;
+  getBooks(searchInput.value);
+})
+
+// Fetch books based on given search term
+const getBooks = (search) => {
+  fetch("http://openlibrary.org/search.json?q=" + search)
+  .then(response => response.json())
+  .then(results => printResults(results))
+  .catch(error => console.log(error));
+}
+
+// Print search results on page
+const printResults = (results) => {
+  console.log(results);
+  
+  for (let i = 0; i <= 10; i++) {
+    const book = document.createElement("img");
+    book.src = "http://covers.openlibrary.org/b/id/" + results.docs[i].cover_i + "-M.jpg";
+    resultsDiv.appendChild(book);
+
+    book.addEventListener("click", () => {
+      console.log(i);
+      localStorage.setItem("Details", results.docs[i]);
+      window.location.href = "searchDetails.html";
+    })
+  }
+}
