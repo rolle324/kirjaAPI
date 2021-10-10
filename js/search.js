e837c3c938f0b69587910059b09e5b2baf277284
rolle324 <jeremiasahonen@hotmail.com>
2021-10-10 14:06:45 +0300

    Responsiivisuus sekä tyylittely

--- js/search.js
+++ js/search.js
@@ -9,88 +9,93 @@ const resultsDiv = document.querySelector("#results");
 
 // Event listener for search button
 searchButton.addEventListener("click", () => {
-  getBooks(searchInput.value);
+    getBooks(searchInput.value);
 })
 
 // Fetch books based on given search term (does not include description and links)
 const getBooks = (search) => {
-  fetch("http://openlibrary.org/search.json?q=" + search)
-  .then(response => response.json())
-  .then(results => printResults(results))
-  .catch(error => console.log(error));
+    fetch("http://openlibrary.org/search.json?q=" + search)
+        .then(response => response.json())
+        .then(results => printResults(results))
+        .catch(error => console.log(error));
 }
 
 // Print search results and buttons on page
 const printResults = (results) => {
-  resultsDiv.innerHTML = ``;
-  console.log(results);
-  
-  for (let i = firstResult; i < lastResult; i++) {
-    if (results.docs[i] != null) {
-      const book = document.createElement("div");
-      book.className = "book";
+    resultsDiv.innerHTML = ``;
+    console.log(results);
 
-      const cover = document.createElement("img");
-      // Use a default cover image if the API doesn't provide one
-      if (results.docs[i].cover_i != null) {
-        cover.src = "http://covers.openlibrary.org/b/id/" + results.docs[i].cover_i + "-M.jpg";
-      } else {
-        cover.src = "\img\navbar\book_icon.png";
-      }
-      cover.alt = "Cover of " + results.docs[i].title;
+    for (let i = firstResult; i < lastResult; i++) {
+        if (results.docs[i] != null) {
+            const book = document.createElement("div");
+            book.className = "book";
 
-      const title = document.createElement("p");
-      title.innerText = results.docs[i].title;
+            const cover = document.createElement("img");
+            // Use a default cover image if the API doesn't provide one
+            if (results.docs[i].cover_i != null) {
+                cover.src = "http://covers.openlibrary.org/b/id/" + results.docs[i].cover_i + "-M.jpg";
+            } else {
+                cover.src = "\img\search\book.png";
+            }
+            cover.alt = "Cover of " + results.docs[i].title;
+            cover.style = "width:100%";
 
-      resultsDiv.appendChild(book);
-      book.appendChild(cover);
-      book.appendChild(title);
+            const title = document.createElement("p");
+            title.innerText = results.docs[i].title;
 
-      // Event listener that adds the book's details to localStorage and moves to the searchDetails page
-      book.addEventListener("click", () => {
-        localStorage.setItem("Key", results.docs[i].key);
+            resultsDiv.appendChild(book);
+            book.appendChild(cover);
+            book.appendChild(title);
 
-        if (results.docs[i].author_name != null) {
-          localStorage.setItem("Author", results.docs[i].author_name[0]);
-        } else {
-          localStorage.setItem("Author", "Author unknown");
+            // Event listener that adds the book's details to localStorage and moves to the searchDetails page
+            book.addEventListener("click", () => {
+                localStorage.setItem("Key", results.docs[i].key);
+
+                if (results.docs[i].author_name != null) {
+                    localStorage.setItem("Author", results.docs[i].author_name[0]);
+                } else {
+                    localStorage.setItem("Author", "Author unknown");
+                }
+
+                if (results.docs[i].first_publish_year != null) {
+                    localStorage.setItem("Published", results.docs[i].first_publish_year);
+                } else {
+                    localStorage.setItem("Published", "Publishing year unknown");
+                }
+
+                window.location.href = "searchDetails.html";
+            })
         }
+    }
 
-        if (results.docs[i].first_publish_year != null) {
-          localStorage.setItem("Published", results.docs[i].first_publish_year);
-        } else {
-          localStorage.setItem("Published", "Publishing year unknown");
+    const buttonelements = document.createElement("div");
+    buttonelements.className = "buttonelements";
+    const previous = document.createElement("button");
+    previous.innerText = "Previous";
+
+    const next = document.createElement("button");
+    next.innerText = "Next";
+
+
+    buttonelements.appendChild(previous);
+    buttonelements.appendChild(next);
+    resultsDiv.appendChild(buttonelements);
+
+    // Event listener that shows the previous 10 books
+    previous.addEventListener("click", () => {
+        if (firstResult >= 10) {
+            firstResult -= 10;
+            lastResult -= 10;
+            printResults(results);
         }
+    })
 
-        window.location.href = "searchDetails.html";
-      })
-    }
-  }
-
-  const previous = document.createElement("button");
-  previous.innerText = "Previous";
-
-  const next = document.createElement("button");
-  next.innerText = "Next";
-
-  resultsDiv.appendChild(previous);
-  resultsDiv.appendChild(next);
-  
-  // Event listener that shows the previous 10 books
-  previous.addEventListener("click", () => {
-    if (firstResult >= 10) {
-      firstResult -= 10;
-      lastResult -= 10;
-      printResults(results);
-    }
-  })
-
-  // Event listener that shows the next 10 books
-  next.addEventListener("click", () => {
-    if (lastResult <= results.docs.length) {
-      firstResult += 10;
-      lastResult += 10;
-      printResults(results);
-    }
-  })
+    // Event listener that shows the next 10 books
+    next.addEventListener("click", () => {
+        if (lastResult <= results.docs.length) {
+            firstResult += 10;
+            lastResult += 10;
+            printResults(results);
+        }
+    })
 }
